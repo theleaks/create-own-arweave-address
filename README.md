94464147b0074eb2e605c5034f75246165dbeaca
Theleaks <admin@bedo.dev>
2022-09-23 11:23:06 +0300

    ⚡

---
+++ README.md
@@ -0,0 +1,20 @@
+# Create Own Arweave Address
+
+## Install
+```sh
+git clone https://github.com/theleaks/create-own-arweave-address.git
+npm i
+```
+## Set Keywords
+There is no case sensitivity. Finds matches in both lowercase and uppercase letters.
+
+Each line is a word.
+- Edit -> keywords.txt
+
+## Start
+```sh
+npm run start
+```
+
+
+
---
+++ app.js
@@ -0,0 +1,25 @@
+const { init } = require('arweave');
+const fs = require('fs');
+
+const arweave = init({
+    host: 'arweave.net',
+    port: 443,
+    protocol: 'https'
+});
+
+const array = fs.readFileSync('keywords.txt').toString().split("\r\n").join("|");
+const regex = new RegExp(`^(${array})`, 'gmi');
+
+const generator = async () => {
+    arweave.wallets.generate().then(async (key) => {
+        await arweave.wallets.jwkToAddress(key).then((address) => {
+            key.address = address;
+            console.log(key.address);
+            regex.test(key.address) ?
+                fs.writeFileSync("./wallet/" +key.address + '.json', JSON.stringify(key, null, 2)) & generator()
+                : generator();
+        });
+    });
+}
+
+generator();
